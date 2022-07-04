6abb2aa1c6b07d1b7fc0bfa7b6a3418cc8cd3bae
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-04 14:58:56 +0300

    Remove junk files

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { add } from '../tests/index';
-
-describe('is working', () => {
-  test('true is true', () => {
-    expect(add(2, 2)).toBe(4);
-  });
-});
-
--- tests/index.ts
+++
@@ -1,5 +0,0 @@
-function add(operand1:number, operand2:number):number {
-  return operand1 + operand2;
-}
-
-export {add}
