5d8c38f3f8899e7115c627137b81b1b1dd17ed83
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-04 14:57:02 +0300

    Create webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-const { normalize } = require("path");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: {
+    index: './src/index.ts',
+  },
   mode: 'development',
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader'
-        }
-      }
-    ]
+      },
+    ],
   },
-
-  devtool: false,
-
+  devtool: 'eval',
   resolve: {
-    extensions: ['.ts', '.js']
-  }
-}
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
