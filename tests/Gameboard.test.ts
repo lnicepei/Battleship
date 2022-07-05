ab813d1054e6a6b1ef1a4cf53840d389b244c2d9
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-05 16:09:44 +0300

    Add more tests

--- tests/Gameboard.test.ts
+++ tests/Gameboard.test.ts
@@ -12,6 +12,41 @@ it('Does the Gameboard function return an object?', () => {
   expect(Gameboard()).toBeInstanceOf(Object);
 });
 
-// describe('Does the Gameboard function return an object?', () => {
-//     expect(Gameboard()).toContain(arrayOfShips);
-// });
+it('Does the returned object have arrayOfShips property?', () => {
+  expect(Gameboard()).toHaveProperty('arrayOfShips');
+});
+
+it('Is the arrayOfShips full of ships?', () => {
+  expect(Gameboard().arrayOfShips.length).toBe(10);
+});
+
+it('Does the returned object have receiveAttack method?', () => {
+  expect(Gameboard()).toHaveProperty('receiveAttack');
+});
+
+it('Does the Gameboard hit the ship?', () => {
+  expect(Gameboard()).toHaveProperty('receiveAttack');
+});
+
+it('Does the Gameboard hit the ship?', () => {
+  expect(Gameboard().receiveAttack(2, 2).whereHit).toEqual(expect.arrayContaining(['x']));
+});// this test was meant to work with static ships
+
+it('How correctly does the Gameboard hit the ship?', () => {
+  expect(Gameboard().receiveAttack(2, 2)).toMatchObject({
+    sunk: true,
+    whereHit: ['x'],
+  });
+});
+
+it('Can player sink ships?', () => {
+  expect(Gameboard().receiveAttack(2, 2).sunk).toBe(true);
+});
+
+it('Are ships placed correctly inside the array?', () => {
+  expect(Gameboard().arrayOfShips[5].length).toBe(3);
+});
+
+it('What is inside the gameboardArray?', () => {
+  expect(Gameboard().gameboardArray).toBe(3);
+});
