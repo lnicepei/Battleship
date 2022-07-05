9ff203c0c67e0d72a92e9a1d662a234cf3e725f7
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-05 16:08:27 +0300

    Build functions to place ships

--- src/Gameboard.ts
+++ src/Gameboard.ts
@@ -3,28 +3,66 @@ import { Ships } from './Ships';
 
 function Gameboard(): {
   arrayOfShips: Ship[];
-  receiveAttack(coordinateX: number, coordinateY: number): void;
+  receiveAttack(coordinateX: number, coordinateY: number): Ship;
+  gameboardArray: number[][];
 } {
-  const arrayOfShips: Ship[] = [];
-
-  for (let i = 4; i > 0; i--) {
-    for (let k = 0; k < i; k++) {
-      arrayOfShips.push(Ships(i, randomCoordinates(), randomCoordinates()));
-    }
-  }
-
+  let arrayOfShips: Ship[] = [];
+  let gameboardArray: number[][] = [];
+  [arrayOfShips, gameboardArray] = placeShips(arrayOfShips);
   return {
     arrayOfShips,
     receiveAttack(coordinateX: number, coordinateY: number) {
-      arrayOfShips.find(
-        (ship) => ship.coordinateX == coordinateX && ship.coordinateY == coordinateY
-      );
+      const attackedShip = arrayOfShips.find((ship) => {
+        if (
+          ship.coordinateX <= coordinateX &&
+          coordinateX < ship.coordinateX + ship.length &&
+          ship.coordinateY == coordinateY
+        )
+          return true;
+      }) as Ship;
+      attackedShip.hit(coordinateX, coordinateY);
+      return attackedShip;
     },
+    gameboardArray,
   };
 }
 
+function placeShips(arrayOfShips: Ship[]): [Ship[], number[][]] {
+  const shipsBoard: number[][] = [[], [], [], [], [], [], [], [], [], []];
+  const cols = 10,
+    rows = 10;
+
+  for (let i = 0; i < cols; i++) {
+    shipsBoard[i] = new Array(rows);
+    for (let j = 0; j < rows; j++) {
+      shipsBoard[i][j] = 0;
+    }
+  }
+
+  for (let k = 1; k <= 4; k++) {
+    for (let i = 1; i <= k; i++) {
+      // arrayOfShips.push(Ships(i, randomCoord inates(), randomCoordinates()));
+      // arrayOfShips.push(Ships(4, randomCoordinates(), randomCoordinates()));
+      const randomX: number = randomCoordinates();
+      const randomY: number = randomCoordinates();
+      const ship = Ships(i, randomX, randomY);
+
+      arrayOfShips.push(ship);
+
+      for (let y = 0; y < 10; y++)
+        for (let x = 0; x < 10; x++) {
+          if (ship.length + x <= 10) shipsBoard[x][y] = 1;
+          // shipsBoard[j][randomY + 1] = 1;
+        }
+    }
+  }
+
+  return [arrayOfShips, shipsBoard];
+}
+
 function randomCoordinates(): number {
-  return Math.floor(Math.random() * 9);
+  return 1 + Math.floor(Math.random() * 9);
+  // return 2;
 }
 
 export { Gameboard };
