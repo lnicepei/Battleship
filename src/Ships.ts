e995746de994a479395a72c35db5b6857d119937
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-05 16:09:31 +0300

    Update hit funciton to receive 2 coordinates

--- src/Ships.ts
+++ src/Ships.ts
@@ -8,18 +8,21 @@ function Ships(
   sunk: boolean;
   coordinateX: number;
   coordinateY: number;
-  hit(parameter: number): void;
+  area: number[];
+  hit(hitX: number, hitY: number): void;
   isSunk(): boolean;
 } {
   return {
     length: length,
-    whereHit: new Array(length).fill(""),
+    whereHit: new Array(length).fill(''),
     sunk: false,
     coordinateX: coordinateX,
     coordinateY: coordinateY,
-    hit(cell: number) {
-      cell < this.length ? (this.whereHit[cell] = 'x') : alert('error');
-      this.sunk = this.whereHit.every((cell) => cell === this.whereHit[0]);
+    area: [coordinateX - 1, coordinateY - 1, coordinateX - 1 + length, coordinateY - 1 + length],
+    hit(hitX: number, hitY: number): void {
+      if (hitY == coordinateY && coordinateX <= hitX && hitX < coordinateX + this.length)
+        this.whereHit[hitX - coordinateX] = 'x';
+      this.sunk = this.whereHit.every((cell) => cell === 'x');
     },
     isSunk() {
       return this.sunk;
