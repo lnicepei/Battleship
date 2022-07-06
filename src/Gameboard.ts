413694b5685d32722bfcf7233e95a7fc6382c99d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-06 14:04:11 +0300

    Place ships with no space around and rotating

--- src/Gameboard.ts
+++ src/Gameboard.ts
@@ -31,6 +31,7 @@ function createShips(arrayOfShips: Ship[]): [Ship[], number[][]] {
   let shipsBoard: number[][] = [[], [], [], [], [], [], [], [], [], []];
   const cols = 10,
     rows = 10;
+  let result = 1;
 
   for (let i = 0; i < cols; i++) {
     shipsBoard[i] = new Array(rows);
@@ -40,93 +41,118 @@ function createShips(arrayOfShips: Ship[]): [Ship[], number[][]] {
   }
 
   for (let k = 1; k <= 4; k++) {
+    //creates ships and does NOT position them
     for (let i = 1; i <= k; i++) {
-      const ship = Ships(i, 1, 1);
+      // it creates random coordinates and then calls checkFreeSpot
+      result = 1;
+      while (result) {
+        const coordinateX = randomCoordinates();
+        const coordinateY = randomCoordinates();
+        const position = horizontalOrVertical();
+        const ship = Ships(i, coordinateX, coordinateY);
 
-      shipsBoard = checkConsequtivePlace(shipsBoard, ship);
-
-      arrayOfShips.push(ship);
-    }
-  }
-
-  return [arrayOfShips, shipsBoard];
-}
-
-function checkConsequtivePlace(shipsBoard: number[][], ship: Ship): number[][] {
-  // let counter = 0;
-  // for (let k = x; k <= 10 - (ship.coordinateX + ship.length + x); k++) {
-  //   if (shipsBoard[k][y] !== 1) counter++;
-  //   if (counter == ship.length) return true;
-  // }
-  for (let x = 0; x < 10; x++) {
-    for (let y = 0; y < 10; y++) {
-      switch (ship.length) {
-        case 1:
-          if (shipsBoard[x][y] !== 1) {
-            shipsBoard[x][y] = 1;
-            shipsBoard[x + 1][y] = 0;
-            ship.coordinateX = x;
-            ship.coordinateY = y;
-            return shipsBoard;
-          }
-          break;
-        case 2:
-          if (shipsBoard[x][y] !== 1 && shipsBoard[x + 1][y] !== 1) {
-            shipsBoard[x][y] = 1;
-            shipsBoard[x + 1][y] = 1;
-            shipsBoard[x + 2][y] = 0;
-            ship.coordinateX = x;
-            ship.coordinateY = y;
-            return shipsBoard;
-          }
-          break;
-        case 3:
-          if (x + 2 < 10) {
-            if (
-              shipsBoard[x][y] !== 1 &&
-              shipsBoard[x + 1][y] !== 1 &&
-              shipsBoard[x + 2][y] !== 1
-            ) {
-              shipsBoard[x][y] = 1;
-              shipsBoard[x + 1][y] = 1;
-              shipsBoard[x + 2][y] = 1;
-              shipsBoard[x + 3][y] = 0;
-              ship.coordinateX = x;
-              ship.coordinateY = y;
-              return shipsBoard;
-            }
-          }
-          break;
-        case 4:
-          if (x + 3 < 10) {
-            if (
-              shipsBoard[x][y] !== 1 &&
-              shipsBoard[x + 1][y] !== 1 &&
-              shipsBoard[x + 2][y] !== 1 &&
-              shipsBoard[x + 3][y] !== 1
-            ) {
-              shipsBoard[x][y] = 1;
-              shipsBoard[x + 1][y] = 1;
-              shipsBoard[x + 2][y] = 1;
-              shipsBoard[x + 3][y] = 1;
-              shipsBoard[x + 4][y] = 0;
-              ship.coordinateX = x;
-              ship.coordinateY = y;
-              return shipsBoard;
-            }
-          }
-          break;
-        default:
-          return shipsBoard;
+        [shipsBoard, result] = checkFreeSpot(shipsBoard, ship, coordinateX, coordinateY, position);
+        if (!result) arrayOfShips.push(ship);
       }
     }
   }
-  return shipsBoard;
+  return [arrayOfShips, shipsBoard];
+}
+
+function checkFreeSpot(
+  shipsBoard: number[][],
+  ship: Ship,
+  x: number,
+  y: number,
+  position: string
+): [number[][], number] {
+  let result = 0;
+
+  switch (ship.length) {
+    case 1:
+      if (shipsBoard[x][y] == 1) {
+        result = 1;
+      } else {
+        ship.coordinateX = x;
+        ship.coordinateY = y;
+        shipsBoard[x][y] = 1;
+        result = 0;
+      }
+      break;
+    case 2:
+      if (x + 1 < 10 && y + 1 < 10) {
+        if (shipsBoard[x][y] == 1 || shipsBoard[x + 1][y] == 1) {
+          result = 1;
+        } else {
+          ship.coordinateX = x;
+          ship.coordinateY = y;
+          shipsBoard[x][y] = 1;
+          position === 'vertical' ? (shipsBoard[x + 1][y] = 1) : (shipsBoard[x][y + 1] = 1);
+          result = 0;
+        }
+      } else {
+        result = 1;
+      }
+      break;
+    case 3:
+      if (x + 2 < 10 && y + 2 < 10) {
+        if (shipsBoard[x][y] == 1 || shipsBoard[x + 1][y] == 1 || shipsBoard[x + 2][y] == 1) {
+          result = 1;
+        } else {
+          ship.coordinateX = x;
+          ship.coordinateY = y;
+          shipsBoard[x][y] = 1;
+          position === 'vertical' ? (shipsBoard[x + 1][y] = 1) : (shipsBoard[x][y + 1] = 1);
+          position === 'vertical' ? (shipsBoard[x + 2][y] = 1) : (shipsBoard[x][y + 2] = 1);
+          result = 0;
+        }
+      } else {
+        result = 1;
+      }
+      break;
+    case 4:
+      if (x + 3 < 10 && y + 3 < 10) {
+        if (
+          shipsBoard[x][y] == 1 ||
+          shipsBoard[x + 1][y] == 1 ||
+          shipsBoard[x + 2][y] == 1 ||
+          shipsBoard[x + 3][y] == 1
+        ) {
+          result = 1;
+        } else {
+          ship.coordinateX = x;
+          ship.coordinateY = y;
+          shipsBoard[x][y] = 1;
+          position === 'vertical' ? (shipsBoard[x + 1][y] = 1) : (shipsBoard[x][y + 1] = 1);
+          position === 'vertical' ? (shipsBoard[x + 2][y] = 1) : (shipsBoard[x][y + 2] = 1);
+          position === 'vertical' ? (shipsBoard[x + 3][y] = 1) : (shipsBoard[x][y + 3] = 1);
+          result = 0;
+        }
+      } else {
+        result = 1;
+      }
+      break;
+    default:
+      break;
+  }
+
+  // if (result == 0) {
+  //   for (let index = x; index < ship.length + x; index++) {
+  //     // check (ship.length) next fields
+  //     shipsBoard[index][y] = 1;
+  //   }
+  // }
+  return [shipsBoard, result];
 }
 
 function randomCoordinates(): number {
   return 1 + Math.floor(Math.random() * 9);
   // return 2;
+}
+
+function horizontalOrVertical(): string { //works for placing either horizontally or vertitally
+  return 'vertical';
+  // return (Math.random() * 100) % 2 == 1 ? 'horizontal' : 'vertical';
 }
 
 export { Gameboard };
