eebde34e522ece2f789e9a9f6f65fd4accc0bedb
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-07 10:57:22 +0300

    Change hit method to string array

--- src/Ships.ts
+++ src/Ships.ts
@@ -19,10 +19,11 @@ function Ships(
     coordinateX: coordinateX,
     coordinateY: coordinateY,
     area: [coordinateX, coordinateY, coordinateX + length, coordinateY + 1],
-    hit(hitX: number, hitY: number): void {
+    hit(hitX: number, hitY: number): string[] {
       if (hitY == coordinateY && coordinateX <= hitX && hitX < coordinateX + this.length)
         this.whereHit[hitX - coordinateX] = 'x';
       this.sunk = this.whereHit.every((cell) => cell === 'x');
+      return this.whereHit;
     },
     isSunk() {
       return this.sunk;
