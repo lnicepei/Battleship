6b48d28b42e3065222b6dcb1dc1b160fb0ff8035
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-09 10:36:24 +0300

    Remove old code

--- src/Gameboard.ts
+++ src/Gameboard.ts
@@ -48,6 +48,7 @@ function createShips(arrayOfShips: Ship[]): [Ship[], number[][]] {
         [shipsBoard, result] = checkBorderCells(shipsBoard, ship);
         if (!result) arrayOfShips.push(ship);
       }
+      console.table(shipsBoard);
     }
   }
   return [arrayOfShips, shipsBoard];
@@ -63,74 +64,15 @@ function checkBorderCells(shipsBoard: number[][], ship: Ship): [number[][], numb
         }
       }
     }
+    for (let x = ship.coordinateX; x < ship.coordinateX + ship.length; x++) {
+      shipsBoard[ship.coordinateY][x] = 1;
+    }
   }
-
-  for (let x = ship.coordinateX; x < ship.coordinateX + ship.length; x++) {
-    shipsBoard[ship.coordinateY][x] = 1;
-  }
-  //   for (let x = ship.coordinateX; x < ship.coordinateX + ship.length; x++) {
-  //     if (
-  //       y - 1 >= 0 &&
-  //       y + 1 < 10 &&
-  //       x - 1 >= 0 &&
-  //       x - 1 < 10 &&
-  //       x >= 0 &&
-  //       x < 10 &&
-  //       y >= 0 &&
-  //       y < 10 &&
-  //       ship.coordinateX + ship.length < 10 &&
-  //       ship.coordinateX - 1 >= 0
-  //     ) {
-  //       if (
-  //         shipsBoard[y - 1][x - 1] == 0 &&
-  //         shipsBoard[y + 1][x - 1] == 0 &&
-  //         shipsBoard[y][ship.coordinateX - 1] == 0 &&
-  //         shipsBoard[y][ship.coordinateX + ship.length] == 0 &&
-  //         shipsBoard[y][x] == 0
-  //       ) {
-  //         shipsBoard[y][x] = 1;
-  //       } else {
-  //         return [initialBoard, 1];
-  //       }
-  //     } else {
-  //       return [initialBoard, 1];
-  //     }
-  //   }
-  // }
-
-  // if (ship.position === 'vertical') {
-  //   const x = ship.coordinateX;
-  //   const initialBoard: number[][] = shipsBoard.map((arr) => arr.slice());
-  //   for (let y = ship.coordinateY; y < ship.coordinateY + ship.length; y++) {
-  //     if (
-  //       y - 1 > 0 &&
-  //       y - 1 < 10 &&
-  //       x - 1 > 0 &&
-  //       x + 1 < 10 &&
-  //       ship.coordinateY + ship.length < 10 &&
-  //       ship.coordinateY - 1 > 0
-  //     ) {
-  //       if (
-  //         shipsBoard[y - 1][x - 1] == 0 &&
-  //         shipsBoard[y - 1][x + 1] == 0 &&
-  //         shipsBoard[ship.coordinateY - 1][ship.coordinateX] == 0 &&
-  //         shipsBoard[ship.coordinateY + ship.length][ship.coordinateX] == 0
-  //       ) {
-  //         shipsBoard[y][x] = 2;
-  //       } else {
-  //         return [initialBoard, 1];
-  //       }
-  //     } else {
-  //       return [initialBoard, 1];
-  //     }
-  //   }
-  // }
-
   return [shipsBoard, 0];
 }
 
 function randomCoordinate(): number {
-  return 1 + Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 9);
 }
 
 function horizontalOrVertical(): string {
