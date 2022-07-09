fac81ab7b07ea8bb9b53fd3c05cfb52ea8db4d60
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-09 16:31:54 +0300

    Automatically decide width and height

--- src/Ships.ts
+++ src/Ships.ts
@@ -5,6 +5,8 @@ function Ships(
   coordinateY: number
 ): {
   length: number;
+  width: number;
+  height: number;
   position: string;
   whereHit: string[];
   sunk: boolean;
@@ -14,14 +16,23 @@ function Ships(
   hit(hitX: number, hitY: number): void;
   isSunk(): boolean;
 } {
+  let width, height;
+  if (position === 'vertical') {
+    width = 1;
+    height = length;
+  } else {
+    width = length;
+    height = 1;
+  }
   return {
     length: length,
+    width: width,
+    height: height,
     position: position,
     whereHit: new Array(length).fill(''),
     sunk: false,
     coordinateX: coordinateX,
     coordinateY: coordinateY,
-    // area: [coordinateX, coordinateY, coordinateX + length, coordinateY + 1],
     hit(hitX: number, hitY: number): string[] {
       if (hitY == coordinateY && coordinateX <= hitX && hitX < coordinateX + this.length)
         this.whereHit[hitX - coordinateX] = 'x';
