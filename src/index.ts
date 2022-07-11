5efd161e0ac8b47cd6ae2e1e47e6bb8f17704ab9
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-11 14:58:21 +0300

    Work on creating players

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { randomCoordinate } from './Gameboard';
+import { createPlayer } from './Player';
+import './style.css';
+
+const human = createPlayer('Dmitry');
+const robot = createPlayer('robot');
+
+let result:boolean = robot.makeMove(randomCoordinate(), randomCoordinate(), human.movesBoard, human.playersGameboard);
+
+export { human };
