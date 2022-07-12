367b5e296f8268d046cd8413a34438c219f73fe7
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-12 15:39:57 +0300

    Adapt test no. 2 to test promises

--- tests/Player.test.ts
+++ tests/Player.test.ts
@@ -4,11 +4,7 @@ it('Check if tests actually work', () => {
   expect(true).toBe(true);
 });
 
-it('check if players can attack one another', () => {
-  expect(
-    human.playersGameboard
-      .receiveAttack(1, 2)
-      .flat()
-      .filter((element: number) => element > 2).length > 0
-  ).toBe(true);
+it('check if players can attack one another', async () => {
+  const array = await human.playersGameboard.receiveAttack(1, 2);
+  expect(array.flat().filter((element: number) => element > 2).length > 0).toBe(true);
 });
