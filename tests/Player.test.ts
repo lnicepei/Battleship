92b9a1142f65373e8ead7284118d2e060c7b776c
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-15 09:50:17 +0300

    Add tests

--- tests/Player.test.ts
+++ tests/Player.test.ts
@@ -1,10 +1,32 @@
-// import { human } from '../src/index';
+import { createPlayer } from '../src/Player';
 
-it('Check if tests actually work', () => {
-  expect(true).toBe(true);
+describe('Check the work of Player factory function', () => {
+  const somePlayer = createPlayer('Dude');
+
+  it('Check if tests actually work', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Check the name to be valid', () => {
+    expect(somePlayer.name).toBe('Dude');
+  });
+
+  it('Check the name of player`s board', () => {
+    expect(somePlayer.playersGameboard.name).toBe('Dude');
+  });
+
+  it('Do players have reset method', () => {
+    somePlayer.reset();
+    expect(somePlayer.name).toBe('');
+  });
+
+  it('Is players` gameboard populated with ships?', () => {
+    expect(
+      somePlayer.playersGameboard.shipsBoard.flat().filter((element) => element > 0).length > 0
+    ).toBe(true);
+  });
+
+  it('Is players` movesboard empty?', () => {
+    expect(somePlayer.movesBoard.flat().filter((element) => element > 0).length == 0).toBe(true);
+  });
 });
-
-// it('check if players can attack one another', async () => {
-//   const array = await human.playersGameboard.receiveAttack(1, 2);
-//   expect(array.flat().filter((element: number) => element > 2).length > 0).toBe(true);
-// });
