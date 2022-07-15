7ae41c00b837fb08d7ac6c604eeda794880a0f79
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-15 15:57:05 +0300

    Add describe part of test

--- tests/Gameboard.test.ts
+++ tests/Gameboard.test.ts
@@ -1,54 +1,64 @@
-import { Ship } from '../src/Gameboard';
+import { finishHim, Ship } from '../src/Gameboard';
 import { Gameboard } from '../src/Gameboard';
 const result = Gameboard('expample');
 
-it('Do tests actually work?', () => {
-  expect(true).toBe(true);
-});
+describe('Test the validity of Gameboard factory', () => {
+  it('Do tests actually work?', () => {
+    expect(true).toBe(true);
+  });
 
-it('Does the Gameboard function return something?', () => {
-  expect(result).toBeDefined();
-});
+  it('Does the Gameboard function return something?', () => {
+    expect(result).toBeDefined();
+  });
 
-it('Does the Gameboard function return an object?', () => {
-  expect(result).toBeInstanceOf(Object);
-});
+  it('Does the Gameboard function return an object?', () => {
+    expect(result).toBeInstanceOf(Object);
+  });
 
-it('Does the returned object have arrayOfShips property?', () => {
-  expect(result).toHaveProperty('arrayOfShips');
-});
+  it('Does the returned object have arrayOfShips property?', () => {
+    expect(result).toHaveProperty('arrayOfShips');
+  });
 
-it('Is the arrayOfShips full of ships?', () => {
-  expect(result.arrayOfShips.length).toBe(10);
-});
+  it('Is the arrayOfShips full of ships?', () => {
+    expect(result.arrayOfShips.length).toBe(10);
+  });
 
-it('Does the returned object have receiveAttack method?', () => {
-  expect(result).toHaveProperty('receiveAttack');
-});
-
-it('Does the Gameboard hit the ship?', () => {
-  expect(result).toHaveProperty('receiveAttack');
-});
-
-it('Does the ship recieve attack?', () => {
-  const ship: Ship = result.arrayOfShips[0];
-  expect(ship.hit(ship.coordinateX, ship.coordinateY)).toMatchObject({
-    whereHit: ['x', '', '', ''],
+  it('Does the returned object have receiveAttack method?', () => {
+    expect(result).toHaveProperty('receiveAttack');
   });
 });
 
-it('Are there enough ship in the arrayOfShips?', () => {
-  expect(result.arrayOfShips.length).toBe(10);
+describe('Check what is inside Gameboard objects', () => {
+  it('Does the ship recieve attack?', () => {
+    const ship: Ship = result.arrayOfShips[0];
+    expect(ship.hit(ship.coordinateX, ship.coordinateY)).toMatchObject({
+      whereHit: ['x', '', '', ''],
+    });
+  });
+
+  it('Are there enough ship in the arrayOfShips?', () => {
+    expect(result.arrayOfShips.length).toBe(10);
+  });
+
+  it('Are ships placed correctly inside the array?', () => {
+    expect(result.arrayOfShips[0].length).toBe(4);
+  });
+
+  it('What is the length of shipsBoard?', () => {
+    expect(result.shipsBoard.flat().length).toBe(100);
+  });
+
+  it('How many ships are there on the board?', () => {
+    expect(result.shipsBoard.flat().filter((element) => element > 0).length).toBe(20);
+  });
 });
 
-it('Are ships placed correctly inside the array?', () => {
-  expect(result.arrayOfShips[0].length).toBe(4);
-});
+describe('Check, the work AI', () => {
+  const someGameboard = Gameboard('Example');
+  const oneCellShip = someGameboard.arrayOfShips[9];
+  console.log(someGameboard.arrayOfShips[9]);
 
-it('What is the length of shipsBoard?', () => {
-  expect(result.shipsBoard.flat().length).toBe(100);
-});
-
-it('How many ships are there on the board?', () => {
-  expect(result.shipsBoard.flat().filter((element) => element > 0).length).toBe(20);
+  it('If ship length was 1', () => {
+    // expect(finishHim(oneCellShip.coordinateX, oneCellShip.coordinateY, someGameboard)).toBe();
+  });
 });
