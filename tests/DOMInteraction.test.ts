5ec5656fe250fdac845e577ee3a8555775577833
lnicepei <dmitrii.shaidorov@gmail.com>
2022-07-15 15:56:20 +0300

    Add a test

---
+++ tests/DOMInteraction.test.ts
@@ -0,0 +1,7 @@
+import { markSunkShip } from '../src/DOMInteraction';
+
+describe('Check the functions of DOMInteraction', () => {
+  it('Does marking ships work?', () => {
+    // expect(finishHim());
+  });
+});
